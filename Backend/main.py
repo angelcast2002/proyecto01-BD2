97aaba809b85e1463d250059c0136270e60a2c77
Diego Morales <77738746+Diego2250@users.noreply.github.com>
2024-02-25 13:23:13 -0600

    create user without pp

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+import uvicorn
 from fastapi import FastAPI, HTTPException, File, UploadFile
 import mongoManager as mm
 from pydantic import BaseModel
@@ -12,7 +13,7 @@ app = FastAPI()
 
 # Definición del modelo de usuario
 class User(BaseModel):
-    _id: str
+    id: str
     password: str
     nombre: str
     apellido: str
@@ -21,16 +22,17 @@ class User(BaseModel):
 
 # Ruta para crear un nuevo usuario
 @app.post("/users/")
-async def create_user(user: User, profile_pic: UploadFile = File(...)):
-    db = mm.connect()
-    users_collection = db["usuarios"]
-    fs = GridFS(db)
+async def create_user(user: User):
+    client = mm.connect()
+    db = client['ProyectoDB2']
+    users_collection = db.usuarios
+    #fs = GridFS(db)
     # Verificar si el usuario ya existe
     if users_collection.find_one({"_id": user.id}):
         raise HTTPException(status_code=400, detail="El usuario ya existe")
 
     # Guardar la imagen en GridFS
-    profile_pic_id = fs.put(profile_pic.file, filename=profile_pic.filename)
+    #profile_pic_id = fs.put(profile_pic.file, filename=profile_pic.filename)
 
 
     # Hashear la contraseña antes de almacenarla
@@ -39,10 +41,16 @@ async def create_user(user: User, profile_pic: UploadFile = File(...)):
     # Insertar el nuevo usuario en la base de datos
     user_data = user.dict()
     user_data["birthdate"] = datetime.strptime(user_data["birthdate"], "%Y-%m-%d")
-    user_data['_id'] = user_data['id']
     user_data['password'] = hashed_password.decode('utf-8')
-    user_data['profilepic'] = profile_pic_id
-    inserted_user = users_collection.insert_one(user_data)
-    mm.disconnect(db)
+    #user_data['profilepic'] = profile_pic_id
+    user_data['_id'] = user_data.pop('id')
 
-    return {"message": "Usuario creado exitosamente", "user_id": str(inserted_user.inserted_id)}
+    inserted_user = users_collection.insert_one(user_data)
+    mm.disconnect(client)
+
+
+    #Return status code 200 and message: "User created"
+    return {"status": 200, "message": "User created"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
