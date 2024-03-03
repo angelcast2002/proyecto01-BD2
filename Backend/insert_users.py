47dca1e0454cd34f3ae500c343cc0c31f5ff9048
Diego Morales <77738746+Diego2250@users.noreply.github.com>
2024-03-03 17:05:32 -0600

    update

--- Backend/insert_users.py
+++ Backend/insert_users.py
@@ -16,27 +16,27 @@ fs = GridFS(db)  # Inicializar GridFS
 nombres = ["Juan", "Carlos", "Pedro", "Luis", "José", "Manuel", "Jesús", "Javier", "Francisco", "Daniel", "Alejandro", "Miguel", "Rafael", "Fernando", "Sergio", "Arturo", "Eduardo", "Jorge", "Ricardo", "Emilio", "Antonio", "Roberto", "Guillermo", "Héctor", "Mauricio", "Rodrigo", "Humberto", "Armando", "Alberto", "Enrique", "Gustavo", "Adrián", "Oscar", "Salvador", "Víctor", "Ernesto", "Fernando", "Raúl", "Martín", "Alfredo", "Leonardo", "Eduardo", "Luis", "Ricardo", "Javier", "Héctor", "Miguel", "Roberto", "Francisco", "José", "Manuel", "Jesús", "Alejandro", "Daniel", "Guillermo", "Rafael", "Sergio", "Arturo", "Fernando", "Eduardo", "Jorge", "Ricardo", "Emilio", "Antonio", "Roberto", "Guillermo", "Héctor", "Mauricio", "Rodrigo", "Humberto", "Armando", "Alberto", "Enrique", "Gustavo", "Adrián", "Oscar", "Salvador", "Víctor", "Ernesto", "Fernando", "Raúl", "Martín", "Alfredo", "Leonardo", "Eduardo", "Luis", "Ricardo", "Javier", "Héctor", "Miguel", "Roberto", "Francisco", "José", "Manuel", "Jesús", "Alejandro", "Daniel", "Guillermo", "Rafael", "Sergio", "Arturo", "Fernando", "Eduardo", "Jorge", "Ricardo", "Emilio", "Antonio", "Roberto", "Guillermo", "Héctor", "Mauricio", "Rodrigo"]
 apellidos = ["González", "Rodríguez", "Gómez", "Fernández", "López", "Díaz", "Martínez", "Pérez", "García", "Sánchez", "Romero", "Sosa", "Álvarez", "Torres", "Ruiz", "Ramírez", "Flores", "Acosta", "Benítez", "Medina", "Herrera", "Suárez", "Aguirre", "Jiménez", "Molina", "Ortiz", "Silva", "Gutiérrez", "Rojas", "Núñez", "Cabrera", "Chávez", "Vargas", "Mendoza", "Ramos", "Blanco", "Méndez", "Guerrero", "Suárez", "Aguirre", "Jiménez", "Molina", "Ortiz", "Silva", "Gutiérrez", "Rojas", "Núñez", "Cabrera", "Chávez", "Vargas", "Mendoza", "Ramos", "Blanco", "Méndez", "Guerrero", "Suárez", "Aguirre", "Jiménez", "Molina", "Ortiz", "Silva", "Gutiérrez", "Rojas", "Núñez", "Cabrera", "Chávez", "Vargas", "Mendoza", "Ramos", "Blanco", "Méndez", "Guerrero", "Suárez", "Aguirre", "Jiménez", "Molina", "Ortiz", "Silva", "Gutiérrez", "Rojas", "Núñez", "Cabrera", "Chávez", "Vargas", "Mendoza", "Ramos", "Blanco", "Méndez", "Guerrero", "Suárez", "Aguirre", "Jiménez", "Molina", "Ortiz", "Silva", "Gutiérrez", "Rojas", "Núñez", "Cabrera", "Chávez", "Vargas", "Mendoza", "Ramos", "Bladimir"]
 fechas_nacimiento = [
-    "1988-04-14", "1989-02-22", "1989-06-07", "1989-09-20", "1998-04-15", "1998-07-29", "1998-11-10", "1999-02-23", "1999-06-08", "1999-09-21",
-    "1990-01-01", "1990-04-15", "1990-07-29", "1990-11-10", "1991-02-23", "1991-06-08", "1991-09-21", "1991-12-31", "1992-04-14", "1992-07-28",
-    "1992-11-09", "1993-02-22", "1993-06-07", "1993-09-20", "1993-12-31", "1994-04-15", "1994-07-29", "1994-11-10", "1995-02-23", "1995-06-08",
-    "1995-09-21", "1995-12-31", "1996-04-14", "1996-07-28", "1996-11-09", "1997-02-22", "1997-06-07", "1997-09-20", "1997-12-31", "1998-04-15",
-    "1999-07-23", "1999-11-05", "2000-02-18", "2000-06-02", "2000-09-15", "2000-12-29", "2001-04-13", "2001-07-27", "2001-11-09", "2002-02-22",
-    "2002-06-07", "2002-09-20", "2002-12-31", "2003-04-14", "2003-07-28", "2003-11-10", "2004-02-23", "2004-06-07", "2004-09-20", "2004-12-31",
-    "2005-04-15", "2005-07-29", "2005-11-10", "2006-02-23", "2006-06-08", "2006-09-21", "2006-12-31", "2007-04-16", "2007-07-30", "2007-11-11"
+    ## Solo fechas del 80 al 80 y del 2008 al 2010. 3 por cada año
+    "1980-01-01", "1980-02-02", "1980-03-03", "2008-01-01", "2008-02-02", "2008-03-03",
+    "1981-01-01", "1981-02-02", "1981-03-03", "2009-01-01", "2009-02-02", "2009-03-03",
+    "1982-01-01", "1982-02-02", "1982-03-03", "2010-01-01", "2010-02-02", "2010-03-03",
+    "1983-01-01", "1983-02-02", "1983-03-03", "1984-01-01", "1984-02-02", "1984-03-03",
+    "1985-01-01", "1985-02-02", "1985-03-03", "1986-01-01", "1986-02-02", "1986-03-03",
+    "1987-01-01", "1987-02-02", "1987-03-03", "1988-01-01", "1988-02-02", "1988-03-03",
+    "1989-01-01", "1989-02-02", "1989-03-03", "1990-01-01", "1990-02-02", "1990-03-03"
+    
 ]
 generos = [False, False, True, True, True, False, True, False, True, False, True, False, True]
-emails = ["gmail.com", "outlook.com", "icloud.com", "yahoo.com", "hotmail.com", "live.com", "aol.com", "protonmail.com", "zoho.com", "gmx.com"]
+emails = ["gmail.com", "outlook.com", "icloud.com", "yahoo.com", "hotmail.com", "live.com", "aol.com", "protonmail.com", "zoho.com", "gmx.com", "uvg.edu.gt", "ufm.gt"]
 contrasenas = ["53h7f9gd", "7r3j5b2k", "9p1s6t8m", "4a2x8c7v", "6z9w4q3e", "1y8h5n2u", "3i5o7l6p", "8f4g2k9d", "2r5v6m3n", "9s7w4q3e", "5b4x3w2d", "2s4d6g8h", "7r5e2w8q", "3q7e9z2x", "8c5v3b1n", "1m8n2b4v", "6c3x9v7b", "4l6o9p1i", "9q8w7e2r", "5p2o4i6u"]
 
-# Ruta a la imagen que quieres subir
-ruta_imagen = r"C:\Users\pacay\Downloads\response.png"
 
 cont = 0
 # Insertar datos aleatorios
 for _ in range(10000):  # Insertar 100 usuarios
     nombre = random.choice(nombres)
     apellido = random.choice(apellidos)
-    email = f"{nombre.lower()[1]}.{apellido.lower()}{random.choice([5, 6, 7, 8, ''])}@{random.choice(emails)}"
+    email = f"{nombre.lower()[1]}.{apellido.lower()}.{random.choice([55, 66, 77, 88, ''])}@{random.choice(emails)}"
     fecha_nacimiento = random.choice(fechas_nacimiento)
     genero = random.choice(generos)
     contrasena = random.choice(contrasenas)
@@ -47,10 +47,6 @@ for _ in range(10000):  # Insertar 100 usuarios
         print("Usuario ya existe")
         continue
     else:
-        # Leer la imagen desde la ruta especificada
-        with open(ruta_imagen, 'rb') as f:
-            # Subir la imagen a GridFS y obtener su ObjectId
-            imagen_id = fs.put(f)
 
         cont += 1
 
@@ -61,7 +57,7 @@ for _ in range(10000):  # Insertar 100 usuarios
             "nombre": nombre,
             "apellido": apellido,
             "birthdate": datetime.strptime(fecha_nacimiento, "%Y-%m-%d"),
-            "profilepic": imagen_id,  # ObjectId de la imagen en GridFS
+            "profilepic": ObjectId('65e212709342dd97c5fda05d'),  # ObjectId de la imagen en GridFS
             "gender": genero
         }))
 
